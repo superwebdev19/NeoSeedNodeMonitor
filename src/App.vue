4ac8335e9ab96983f187a57b587eefb355fcbdf4
superwebdev19 <vladislavtsbk@outlook.com>
2019-10-31 20:01:17 +1000

    Added navbar with BootstrapVue

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <!-- <div id="header-nav" class="nav-bars"> -->
+        <!-- <router-link to="/">Home</router-link> |
+        <router-link to="/words">Words</router-link> |
+        <router-link :to="latestWord">{{ latestWord }}</router-link> -->
+    <!-- </div> -->
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-navbar-brand href="/" class="ml-5">NEO Seed Nodes Status Test</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="mx-5">
+            <b-nav-item href="/" class="px-2">Main</b-nav-item>
+            <b-nav-item href="/nodeinfo" class="px-2">Seed Node</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view/>
+    <!-- <div id="footer-nav" class="nav-bars">
+      My Dictionary 2018 - {{ wordCount }} words has been added.
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      wordCount: 0,
+      latestWord: 'Latest_Word'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  overflow: hidden;
+  a {
+    color: royalblue;
+  }
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  // .nav-bars{
+  //   background: cornflowerblue;
+  //   position: fixed;
+  //   width: 100%;
+  //   padding: 10px;
+  //   color: white;
+
+  //   a {
+  //     color: white;
+  //     text-decoration: none;
+  //   }
+  // }
+
+  // #header-nav {
+  //   top: 0;
+  //   left: 0;
+  // }
+
+  #footer-nav {
+    bottom: 0;
+    left: 0;
+  }
+
+  .nav-link {
+    color: white !important;
+  }
+
+  .nav-link:hover {
+    color: orange !important;
+  }
+}
+</style>
