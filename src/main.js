d075820bce92899cc82e3623e09a824bffc61c23
superwebdev19 <vladislavtsbk@outlook.com>
2019-11-11 13:32:27 +1000

    Updated environment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
 import App from './App'
 import Vuex from 'vuex'
@@ -13,11 +13,8 @@ Vue.config.productionTip = false
 Vue.use(Vuex)
 Vue.use(BootstrapVue)
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  store,
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
