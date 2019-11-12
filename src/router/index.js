2ddf833300952a75516b8f64d0a19eb754ab329d
superwebdev19 <superwebdev19@outlook.com>
2019-11-12 17:19:00 +0100

    Updated styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.min.css'
 import Home from '@/components/Home'
 import NodeInfo from '@/components/NodeInfo'
 import Statistics from '@/components/Statistics'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
 
 Vue.use(Router)
 
